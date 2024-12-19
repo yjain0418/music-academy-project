583e2b230b0f54f9c3ab30b7f08206de70f8dac6
yjain0418 <12212162@nitkkr.ac.in>
2024-12-19 17:48:41 +0530

    Contact Us done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -51,6 +51,7 @@ export default {
   theme: {
     extend: {
       animation: {
+        "meteor-effect": "meteor 5s linear infinite",
         spotlight: "spotlight 2s ease .75s 1 forwards",
         scroll:
           "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
@@ -69,6 +70,14 @@ export default {
         scroll: {
           to: {
             transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
           },
         },
       },
