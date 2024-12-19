c0be18bc2314475b593fe084e4a31cfa2f9c47e7
yjain0418 <12212162@nitkkr.ac.in>
2024-12-19 17:07:09 +0530

    CSS updated

--- src/components/TestimonialCards.tsx
+++ src/components/TestimonialCards.tsx
@@ -40,7 +40,7 @@ function TestimonialCards() {
             <h2 className="text-3xl font-bold text-center mb-8 z-10">
                 Hear our Harmony : Voices of Success</h2>
             <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
-                <div className="w-full max-w-6xl">
+                <div className="w-full max-w-8xl">
                     <InfiniteMovingCards
                         items={musicSchoolTestimonials}
                         direction="right"
