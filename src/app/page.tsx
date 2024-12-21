f35611ecb66a0651350d87e7bb71721a5a024446
yjain0418 <12212162@nitkkr.ac.in>
2024-12-21 13:19:35 +0530

    Minor updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import Instructors from "@/components/Instructors";
 import TestimonialCards from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
-import Image from "next/image";
 
 export default function Home() {
   return (
